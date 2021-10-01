3f59a93ab70d36ff55620377376633e76494edfa
TylerWray2 <TylerWray2@users.noreply.github.com>
2021-10-01 17:05:36 -0400

    Use keystate instead of asynckeystate, works better. Basic method for multiple keys.

--- WCInputHandler.cpp
+++ WCInputHandler.cpp
@@ -4,19 +4,44 @@
 #include <winuser.h>
 #include <chrono>
 #include <thread>
+#include "KeyDownEvent.h"
+#include <future>
 
 using namespace std;
 
 chrono::milliseconds REST_TIME(50);
 
 int main(int argc, char* argv[]) {
-    while (true) {
-        int keyCode = stoi(string(argv[1]));
-        if (GetAsyncKeyState(keyCode) & 0x01) {
-            return 0;
-        }
-        this_thread::sleep_for(REST_TIME);
-    }
+        int numKeysInBind = argc - 1;
+        int* keycode = new int[numKeysInBind];
 
+        for (int i = 0; i < numKeysInBind; i++) {
+            keycode[i] = stoi(string(argv[i + 1]));
+        }
+        bool press = false;
+        switch (numKeysInBind)
+        {
+        case 1:
+            while (!press) {
+                if (GetKeyState(keycode[0]) & 0x01) {
+                    press = true;
+                    cout << "Keypress detected" << endl;
+                }
+                this_thread::sleep_for(REST_TIME);
+            }
+        case 2:
+            while (!press) {
+                if ((GetKeyState(keycode[0]) & 0x8000) && (GetKeyState(keycode[1]) & 0x8000)) {
+                    press = true;
+                    cout << "Keypress detected" << endl;
+                }
+                this_thread::sleep_for(REST_TIME);
+            }
+        default:
+            break;
+        }
+    
     return 1;
 }
+
+//handle multiple button presses with 4 bools
